cc4141c9d1b080993f8a6acdf1d520c282f302f7
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2025-06-08 14:36:17 +0700

    Revert "revert sql to case insensitive"
    This reverts commit cadcde24546e10e5e0d1db835a059982130d093a.

--- tables/mysql/sessionBase.sql
+++ tables/mysql/sessionBase.sql
@@ -3,11 +3,11 @@
 # Table structure for table 'memberSessions'
 #
 
-CREATE TABLE memberSessions (
-  idxNum SERIAL PRIMARY KEY,
-  uID CHAR(32) NOT NULL UNIQUE,
-  sID CHAR(32) NOT NULL UNIQUE,
-  dateCreated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE "memberSessions" (
+  "idxNum" SERIAL PRIMARY KEY,
+  "uID" CHAR(32) NOT NULL UNIQUE,
+  "sID" CHAR(32) NOT NULL UNIQUE,
+  "dateCreated" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 
@@ -15,17 +15,17 @@ CREATE TABLE memberSessions (
 # Table structure for table 'members'
 #
 CREATE TABLE members (
-  idxNum SERIAL PRIMARY KEY,
-  uID CHAR(32) NOT NULL,
-  userName VARCHAR(30) NOT NULL DEFAULT '',
-  passWord VARCHAR(30) NOT NULL DEFAULT '',
-  emailAddress VARCHAR(60) NOT NULL DEFAULT '',
-  firstName VARCHAR(50),
-  lastName VARCHAR(50),
-  dateCreated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT unique_email UNIQUE (emailAddress),
-  CONSTRAINT unique_uid UNIQUE (uID),
-  CONSTRAINT unique_username UNIQUE (userName)
+  "idxNum" SERIAL PRIMARY KEY,
+  "uID" CHAR(32) NOT NULL,
+  "userName" VARCHAR(30) NOT NULL DEFAULT '',
+  "passWord" VARCHAR(30) NOT NULL DEFAULT '',
+  "emailAddress" VARCHAR(60) NOT NULL DEFAULT '',
+  "firstName" VARCHAR(50),
+  "lastName" VARCHAR(50),
+  "dateCreated" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT "unique_email" UNIQUE ("emailAddress"),
+  CONSTRAINT "unique_uid" UNIQUE ("uID"),
+  CONSTRAINT "unique_username" UNIQUE ("userName")
 );
 
 
@@ -34,14 +34,14 @@ CREATE TABLE members (
 #
 
 CREATE TABLE sessions (
-  idxNum SERIAL PRIMARY KEY,
-  sessionID VARCHAR(32) NOT NULL DEFAULT '',
-  hashID VARCHAR(32) NOT NULL DEFAULT '',
-  createStamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  remoteHost VARCHAR(20),
-  dataKey VARCHAR(255) NOT NULL DEFAULT '',
-  dataVal TEXT,
-  UNIQUE (hashID)
+  "idxNum" SERIAL PRIMARY KEY,
+  "sessionID" VARCHAR(32) NOT NULL DEFAULT '',
+  "hashID" VARCHAR(32) NOT NULL DEFAULT '',
+  "createStamp" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "remoteHost" VARCHAR(20),
+  "dataKey" VARCHAR(255) NOT NULL DEFAULT '',
+  "dataVal" TEXT,
+  UNIQUE ("hashID")
 );
-CREATE INDEX createStamp_idx ON sessions (createStamp);
-CREATE INDEX sessionID_idx ON sessions (sessionID);
+CREATE INDEX "createStamp_idx" ON sessions ("createStamp");
+CREATE INDEX "sessionID_idx" ON sessions ("sessionID");
