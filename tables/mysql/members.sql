cadcde24546e10e5e0d1db835a059982130d093a
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2025-06-08 10:18:13 +0700

    revert sql to case insensitive

--- tables/mysql/members.sql
+++ tables/mysql/members.sql
@@ -2,15 +2,15 @@
 # Table structure for table 'members'
 #
 CREATE TABLE members (
-  "idxNum" SERIAL PRIMARY KEY,
-  "uID" CHAR(32) NOT NULL,
-  "userName" VARCHAR(30) NOT NULL DEFAULT '',
-  "passWord" VARCHAR(30) NOT NULL DEFAULT '',
-  "emailAddress" VARCHAR(60) NOT NULL DEFAULT '',
-  "firstName" VARCHAR(50),
-  "lastName" VARCHAR(50),
-  "dateCreated" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  UNIQUE ("emailAddress"),
-  UNIQUE ("uID"),
-  UNIQUE ("userName")
+  idxNum SERIAL PRIMARY KEY,
+  uID CHAR(32) NOT NULL,
+  userName VARCHAR(30) NOT NULL DEFAULT '',
+  passWord VARCHAR(30) NOT NULL DEFAULT '',
+  emailAddress VARCHAR(60) NOT NULL DEFAULT '',
+  firstName VARCHAR(50),
+  lastName VARCHAR(50),
+  dateCreated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE (emailAddress),
+  UNIQUE (uID),
+  UNIQUE (userName)
 );
--- tables/mysql/sessions.sql
+++ tables/mysql/sessions.sql
@@ -9,15 +9,15 @@
 #
 
 CREATE TABLE sessions (
-  "idxNum" SERIAL PRIMARY KEY,
-  "sessionID" VARCHAR(32) NOT NULL DEFAULT '',
-  "hashID" VARCHAR(32) NOT NULL DEFAULT '',
-  "createStamp" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "remoteHost" VARCHAR(20),
-  "dataKey" VARCHAR(255) NOT NULL DEFAULT '',
-  "dataVal" TEXT,
-  UNIQUE ("hashID")
+  idxNum SERIAL PRIMARY KEY,
+  sessionID VARCHAR(32) NOT NULL DEFAULT '',
+  hashID VARCHAR(32) NOT NULL DEFAULT '',
+  createStamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  remoteHost VARCHAR(20),
+  dataKey VARCHAR(255) NOT NULL DEFAULT '',
+  dataVal TEXT,
+  UNIQUE (hashID)
 );
 
-CREATE INDEX "createStamp_idx" ON sessions ("createStamp");
-CREATE INDEX "sessionID_idx" ON sessions ("sessionID");
+CREATE INDEX createStamp_idx ON sessions (createStamp);
+CREATE INDEX sessionID_idx ON sessions (sessionID);
